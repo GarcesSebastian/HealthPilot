066473f43f27e4bef5e7f27e05770bb81eb9d864
GarcesSebastian <sebastiangarces152@gmail.com>
2023-10-20 04:02:58 -0500

    initial-commit

--- public/page/home/script.js
+++ public/page/home/script.js
@@ -1,74 +1,104 @@
 let buttonsFooter = document.querySelectorAll(".buttonFooter");
 
 buttonsFooter.forEach((element) => {
-    element.addEventListener("click", () => {
-
-        if(element.getAttribute("data-id") == "1"){
-            window.location.href = "../home/index.html";
-        }else if(element.getAttribute("data-id") == "2"){
-            window.location.href = "../add/index.html";
-        }else if(element.getAttribute("data-id") == "3"){
-            window.location.href = "../calendary/index.html";
-        }
-        
-    });
+  element.addEventListener("click", () => {
+    if (element.getAttribute("data-id") == "1") {
+      window.location.href = "../home/index.html";
+    } else if (element.getAttribute("data-id") == "2") {
+      window.location.href = "../add/index.html";
+    } else if (element.getAttribute("data-id") == "3") {
+      window.location.href = "../calendary/index.html";
+    }
+  });
 });
 
 let buttonsContentPage = document.querySelectorAll(".item");
 
-buttonsContentPage.forEach((element) =>{
-    element.addEventListener('click',()=>{
-        if(element.getAttribute("data-item") == 1){
-            let circle = element.querySelector(".itemCircle");
-            circle.querySelector(".fa-circle").classList.remove("fa-regular");
-            circle.querySelector(".fa-circle").classList.add("fa-solid");
-            setTimeout(()=>{
-                circle.querySelector(".fa-circle").classList.add("fa-regular");
-                circle.querySelector(".fa-circle").classList.remove("fa-solid");      
-            }, 1000)
-        }else if(element.getAttribute("data-item") == 2){
-            let circle = element.querySelector(".itemCircle");
-            circle.querySelector(".fa-circle").classList.remove("fa-regular");
-            circle.querySelector(".fa-circle").classList.add("fa-solid");
-            setTimeout(()=>{
-                circle.querySelector(".fa-circle").classList.add("fa-regular");
-                circle.querySelector(".fa-circle").classList.remove("fa-solid");      
-            }, 1000)
-        }else if(element.getAttribute("data-item") == 3){
-            let circle = element.querySelector(".itemCircle");
-            circle.querySelector(".fa-circle").classList.remove("fa-regular");
-            circle.querySelector(".fa-circle").classList.add("fa-solid");
-            setTimeout(()=>{
-                circle.querySelector(".fa-circle").classList.add("fa-regular");
-                circle.querySelector(".fa-circle").classList.remove("fa-solid");      
-            }, 1000)
-        }else if(element.getAttribute("data-item") == 4){
-            let circle = element.querySelector(".itemCircle");
-            circle.querySelector(".fa-circle").classList.remove("fa-regular");
-            circle.querySelector(".fa-circle").classList.add("fa-solid");
-            setTimeout(()=>{
-                circle.querySelector(".fa-circle").classList.add("fa-regular");
-                circle.querySelector(".fa-circle").classList.remove("fa-solid");      
-            }, 1000)
-        }
-    });
-})
+buttonsContentPage.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (element.getAttribute("data-item") == 1) {
+      let circle = element.querySelector(".itemCircle");
+      circle.querySelector(".fa-circle").classList.remove("fa-regular");
+      circle.querySelector(".fa-circle").classList.add("fa-solid");
+      setTimeout(() => {
+        circle.querySelector(".fa-circle").classList.add("fa-regular");
+        circle.querySelector(".fa-circle").classList.remove("fa-solid");
+      }, 1000);
+    } else if (element.getAttribute("data-item") == 2) {
+      let circle = element.querySelector(".itemCircle");
+      circle.querySelector(".fa-circle").classList.remove("fa-regular");
+      circle.querySelector(".fa-circle").classList.add("fa-solid");
+      setTimeout(() => {
+        circle.querySelector(".fa-circle").classList.add("fa-regular");
+        circle.querySelector(".fa-circle").classList.remove("fa-solid");
+      }, 1000);
+    } else if (element.getAttribute("data-item") == 3) {
+      let circle = element.querySelector(".itemCircle");
+      circle.querySelector(".fa-circle").classList.remove("fa-regular");
+      circle.querySelector(".fa-circle").classList.add("fa-solid");
+      setTimeout(() => {
+        circle.querySelector(".fa-circle").classList.add("fa-regular");
+        circle.querySelector(".fa-circle").classList.remove("fa-solid");
+      }, 1000);
+    } else if (element.getAttribute("data-item") == 4) {
+      let circle = element.querySelector(".itemCircle");
+      circle.querySelector(".fa-circle").classList.remove("fa-regular");
+      circle.querySelector(".fa-circle").classList.add("fa-solid");
+      setTimeout(() => {
+        circle.querySelector(".fa-circle").classList.add("fa-regular");
+        circle.querySelector(".fa-circle").classList.remove("fa-solid");
+      }, 1000);
+    }
+  });
+});
 
 let buttonConfig = document.querySelector(".configSpan");
-let buttonNotification = document.querySelector(".notificationSpan");
-
-
-buttonConfig.addEventListener("click", () =>{
-    document.querySelector(".spawnConfiguracion").style.left = "0%";
-});
-
-buttonNotification.addEventListener("click", () =>{
-    
-});
-
-
 let buttonBackConfig = document.querySelector(".backContent");
+let buttonNotification = document.querySelector(".notificationSpan");
+let buttonBackNotification = document.querySelector(
+  ".backContentNotificaciones"
+);
 
-buttonBackConfig.addEventListener("click", () =>{
-    document.querySelector(".spawnConfiguracion").style.left = "-100%";
+buttonConfig.addEventListener("click", () => {
+  document.querySelector(".spawnConfiguracion").style.left = "0%";
 });
+
+buttonNotification.addEventListener("click", () => {
+  document.querySelector(".spawnNotificaciones").style.right = "0%";
+});
+
+buttonBackNotification.addEventListener("click", () => {
+  document.querySelector(".spawnNotificaciones").style.right = "-100%";
+});
+
+buttonBackConfig.addEventListener("click", () => {
+  document.querySelector(".spawnConfiguracion").style.left = "-100%";
+});
+
+//Spawn Notificaciones
+
+let buttonsTrayNotifications = document.querySelectorAll(".itemTray");
+let contClicksTrayNotifications = 0;
+
+buttonsTrayNotifications.forEach((element) => {
+    if(!element.classList.contains("liActive")){
+        element.addEventListener("click", () => {
+            buttonsTrayNotifications.forEach((element) => {
+              element.style.backgroundColor = "rgb(255, 255, 255, .2)";
+            });
+        
+            element.style.backgroundColor = "rgba(69, 69, 69, 0.5)";
+          });
+    }else{
+        element.addEventListener("click", () =>{
+            contClicksTrayNotifications++;
+            if(contClicksTrayNotifications % 2 != 0){
+                element.querySelector(".arrow").style.transform = "rotate(-180deg)";
+            }else{
+                element.querySelector(".arrow").style.transform = "rotate(0deg)";
+            }
+        });
+    }
+});
+
+//Spawn Notificaciones
