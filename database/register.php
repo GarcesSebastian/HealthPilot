ba47ba1d757dd8c348013cffcaabcc9dce06221f
GarcesSebastian <sebastiangarces152@gmail.com>
2023-10-29 02:27:48 -0500

    initial-commit

--- database/register.php
+++ database/register.php
@@ -15,25 +15,31 @@ if (isset($_POST['buttonSingUp'])) {
             if (mysqli_num_rows($result) > 0) {
                 echo "El nombre de usuario ya está en uso. Por favor, elige otro.";
             } else {
-                $id = rand(0,9999);
-                $state = false;
-
-                $sendData = "INSERT INTO registros ( id , usuario , contraseña , email ) VALUES ('$id','$username','$password','$email')";
-                $sql = mysqli_query($conex, $sendData);
-
-                $sendState = "INSERT INTO estados ( id , estado) VALUES ('$id','$state')";
-                $sqlSendState = mysqli_query($conex, $sendState);
-
-                $sendInfoMedica = "INSERT INTO informacion_medica (id, email) VALUES ('$id','$email')";
-                $sqlSendInfoMedica = mysqli_query($conex,$sendInfoMedica);
-
-                if($sql and $sqlSendState and $sqlSendInfoMedica){
-                    echo "Registrado exitosamente";
-                    header("Location: ../login/index.php");
+                $confirmEmail = "SELECT * FROM registros WHERE email = '$email'";
+                $resultConfirmEmail = mysqli_query($conex, $resultConfirmEmail);
+                
+                if(mysqli_num_rows($resultConfirmEmail)>0){
+                    echo "Este correo electrónico ya se encuentra registrado. Inicia sesión o utiliza uno diferente.";
                 }else{
-                    echo $sql;
+                    $id = rand(0,9999);
+                    $state = false;
+    
+                    $sendData = "INSERT INTO registros ( id , usuario , contraseña , email ) VALUES ('$id','$username','$password','$email')";
+                    $sql = mysqli_query($conex, $sendData);
+    
+                    $sendState = "INSERT INTO estados ( id , estado) VALUES ('$id','$state')";
+                    $sqlSendState = mysqli_query($conex, $sendState);
+    
+                    $sendInfoMedica = "INSERT INTO informacion_medica (id, email) VALUES ('$id','$email')";
+                    $sqlSendInfoMedica = mysqli_query($conex,$sendInfoMedica);
+    
+                    if($sql and $sqlSendState and $sqlSendInfoMedica){
+                        echo "Registrado exitosamente";
+                        header("Location: ../login/index.php");
+                    }else{
+                        echo $sql;
+                    }    
                 }
-
             }
         } else {
             echo "El nombre de usuario debe tener 18 caracteres como máximo.";
